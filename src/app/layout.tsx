8ef05c039375018dce9ac57a8efaf8a68bc00f8d
ulkertehmezova1 <ulkrthmzova@gmaiil.com>
2023-11-06 15:02:27 +0400

    sdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ const acorn = localFont({
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={`${poppins.variable} ${acorn.variable} h-full`}>
-      <body className="text-header bg-bgBody min-h-[100vh] h-full flex flex-col justify-between overflow-x-hidden">
+    <html lang="en" className={`${poppins.variable} ${acorn.variable} flex h-full`}>
+      <body className="text-header bg-bgBody min-h-[100vh] h-full flex flex-col justify-between overflow-x-hidden max-w-full grow">
         <div className="body-container">
           <Header />
           <div className="relative flex flex-col">{children}</div>
