313ea8720f45375884b7008276f2068fe10ffab8
ulkertehmezova1 <ulkrthmzova@gmaiil.com>
2023-12-04 14:23:58 +0400

    work elave olundu

--- src/mocks/my-works.tsx
+++ src/mocks/my-works.tsx
@@ -5,4 +5,10 @@ export const myWorks = [
     startDate: " \n20.11.2023",
     path: "https://app.powerbi.com/reportEmbed?reportId=57804d45-eed1-4e4c-850b-095a517abeaf&autoAuth=true&ctid=a01ffad7-139b-4bd4-acb0-770415bcfbac",
   },
+  {
+    id: 2,
+    name: "Simple report for company payroll information",
+    startDate: " \n28.11.2023",
+    path: "https://app.powerbi.com/reportEmbed?reportId=21486944-f622-4c93-8155-2274f891e046&autoAuth=true&ctid=a01ffad7-139b-4bd4-acb0-770415bcfbac",
+  },
 ];
