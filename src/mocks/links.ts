42fb28517a82697b66c3afea177c9af862ab23a8
ulkertehmezova1 <ulkrthmzova@gmaiil.com>
2024-01-17 12:31:16 +0400

    first_commit

--- src/mocks/links.ts
+++ src/mocks/links.ts
@@ -2,5 +2,5 @@ export const links = [
   { id: 1, href: "/", name: "Work" },
   { id: 2, href: "/about", name: "About" },
   { id: 4, href: "/blog", name: "Posts" },
-  { id: 5, href: "/contact", name: "Contactt" },
+  { id: 5, href: "/contact", name: "Contact" },
 ];
