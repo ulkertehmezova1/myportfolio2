4e4d07aea70ac1b9c0f4e8e4cb5db4e35526de40
ulkertehmezova1 <ulkrthmzova@gmaiil.com>
2024-01-17 15:23:28 +0400

    e

--- src/mocks/skills.ts
+++ src/mocks/skills.ts
@@ -3,37 +3,37 @@ export const skills = [
     id: 1,
     name: "HTML",
     description: "Developed to create structured and semantically meaningful documents ",
-    value: 85,
+    value: 100,
   },
   {
     id: 2,
     name: "CSS",
     description: "Fundamental technology in web development that complements HTML by providing a mechanism  ",
-    value: 60,
+    value: 80,
   },
   {
     id: 3,
     name: "JAVASCRIPT",
     description: "Versatile and dynamic programming language that plays a central role in modern web development",
-    value: 60,
+    value: 70,
   },
   {
     id: 4,
     name: "REACT",
     description:
       "Popular JavaScript library for building user interfaces, developed and maintained by Facebook.",
-    value: 60,
+    value: 90,
   },
   {
     id: 5,
     name: "NEXT JS",
     description: "Open-source React framework that enables to build server-rendered React applications easily. .",
-    value: 50,
+    value: 70,
   },
   {
     id: 9,
     name: "RESPONSIVE DESIGN",
     description:"Web design approach focused on creating web pages that provide an optimal viewing. ",
-    value: 50,
+    value: 80,
   },
 ];
